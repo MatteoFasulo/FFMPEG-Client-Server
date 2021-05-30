46b7531149428d40727c2f8587738a6aec0deb2b
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2021-05-30 19:14:40 +0200

    Update client_latest.py

--- client_latest.py
+++ client_latest.py
@@ -5,10 +5,10 @@ import argparse
 import os
 from socket import *
 import tqdm
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter import Tk  # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 
-serverName = "192.168.1.100"
+serverName = "51.91.254.112"
 BUFFER_SIZE = 1024 * 4
 encoding = 'utf-8'
 
@@ -19,12 +19,12 @@ def tcp_echo_server(filename, verbose):
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
     path, filename = os.path.split(filename)
-    if path!='':
+    if path != '':
         os.chdir(path)
     clientSocket.send(f"{filename}".encode(encoding=encoding))
     filesize = os.path.getsize(filename)
 
-    print("test",f"{path}", filename, sep="\t")
+    print("test", f"{path}", filename, sep="\t")
 
     if verbose:
         progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
@@ -38,7 +38,7 @@ def tcp_echo_server(filename, verbose):
                 progress.update(len(l))
         video.close()
         clientSocket.shutdown(SHUT_WR)
-    
+
     filename, filesize = clientSocket.recv(BUFFER_SIZE).decode().split("SEPARATOR")
     if not os.path.isdir("Download"):
         os.mkdir("Download")
@@ -46,7 +46,8 @@ def tcp_echo_server(filename, verbose):
         if verbose:
             print("\n\n\n")
             progress.reset(total=None)
-            progress = tqdm.tqdm(range(int(filesize)), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+            progress = tqdm.tqdm(range(int(filesize)), f"Receiving {filename}", unit="B", unit_scale=True,
+                                 unit_divisor=1024)
         while True:
 
             data = clientSocket.recv(BUFFER_SIZE)
@@ -62,45 +63,6 @@ def tcp_echo_server(filename, verbose):
         clientSocket.shutdown(SHUT_RDWR)
 
 
-def udp_echo_server():
-    serverPort = 15000
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    message = input("Input lowercase sentence:")
-    while message.lower().strip() != "bye":
-        clientSocket.sendto(bytes(message, encoding=encoding), (serverName, serverPort))
-        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-        print("Received from server: ", modifiedMessage.decode(encoding))
-        message = input("Input lowercase sentence:")
-    clientSocket.shutdown(SHUT_WR)
-
-
-def protocol_pool():
-    key = input("Input: ")
-    diz = {0: "EXIT", 1: "TCP", 2: "UDP"}
-    done = False
-    while not done:
-        try:
-            key = int(key)
-            if key in diz.keys():
-                done = True
-                return diz[key]
-            else:
-                print("Non so cosa fare")
-        except ValueError:
-            if key in diz.values() and key != 0:
-                return key
-            else:
-                break
-
-
-def chose_socket(key):
-    serverPort = 11000
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
-    sentence = key
-    clientSocket.send(bytes(sentence, encoding=encoding))
-
-
 def parse_args():
     arg_error = False
     parser = argparse.ArgumentParser(description='Description of your program', exit_on_error=False)
@@ -113,16 +75,17 @@ def parse_args():
     if arg_error:
         args.target_file = None
     return [args.target_file, args.verbose]
-    
+
 
 def main():
     filename, verbose = parse_args()
-    print(filename,verbose, sep="\t")
-    if filename==None:
-        Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-        filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-        print(filename,verbose, sep="\t")
-        if verbose==None:
+    verbose = True
+    print(filename, verbose, sep="\t")
+    if filename is None:
+        Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+        filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+        print(filename, verbose, sep="\t")
+        if verbose is None:
             verbose = True
     tcp_echo_server(filename, verbose=verbose)
 
